a5b09d7c65fe7ec57698f5832e8ca08accbb2a67
Stanislav Ovcharenko <ovcharenko.s.a.1995@gmail.com>
2023-03-21 19:10:32 +0200

    delete function add manager, elimination of possible errors,and other minor changes

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -1,25 +1,27 @@
-import os
-
+import os, ast
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext, Dispatcher
+from sqlalchemy.exc import IntegrityError
 
 from app.handlers.handlers_commands import admin_commands
 from app.create_bot import bot
-from app.handlers.state_machines import AddManagers, MakeMailing, AddPigment, AddCreator
-from app.keyboards.admin_keyboards import manager_keyboard
+from app.handlers.state_machines import MakeMailing, AddPigment, AddCreator
+from app.keyboards.admin_keyboards import manager_keyboard, cancel_markup_admin
 from app.keyboards.client_keyboards import start_menu
 from app.keyboards.inline import delete_creator_markup
-from app.data_base import AllManagers, session, AllClients, DataMailing, Pigments, Creator
+from app.data_base import session, AllClients, DataMailing, Pigments, Creator
 
-managers_id = []
-direction = ['Татту', 'Перманент']
+managers_id_str = os.getenv('MANAGERS_ID')
+managers_id = ast.literal_eval(managers_id_str)
+
+direction_pigment = ['Татту', 'Перманент']
 
 
-async def start_add_manager(message: types.Message):
-    if int(message.from_user.id) == int(os.getenv('SUPER_USER_ID')):
-        await AddManagers.last_name.set()
-        await message.reply('Введіть прізвище працівника')
+async def start_make_mailing(message: types.Message):
+    if int(message.from_user.id) in managers_id:
+        await MakeMailing.photo.set()
+        await message.reply('Завантажте фото', reply_markup=cancel_markup_admin())
 
 
 # break state machines
@@ -29,39 +31,7 @@ async def cancel_admin_handlers(message: types.Message, state: FSMContext):
     if current_state is None:
         return
     await state.finish()
-    await message.reply('OK')
-
-
-async def add_last_name_manager(message: types.Message, state: FSMContext):
-    if int(message.from_user.id) == int(os.getenv('SUPER_USER_ID')):
-        async with state.proxy() as data:
-            data['last_name'] = message.text
-        await AddManagers.next()
-        await message.reply('Введіть ID працівника')
-
-
-async def add_id_manager(message: types.Message, state: FSMContext):
-    if int(message.from_user.id) == int(os.getenv('SUPER_USER_ID')):
-        async with state.proxy() as data:
-            data['manager_id'] = int(message.from_user.id)
-        if data['manager_id'] in managers_id:
-            await bot.send_message(message.from_user.id, 'Працівник вже в базі')
-        else:
-            managers_id.append(data['manager_id'])
-            new_manager = AllManagers(last_name=data['last_name'], manager_telegram_id=int(data['manager_id']))
-            try:
-                session.add(new_manager)
-                session.commit()
-            except:
-                pass
-
-        await state.finish()
-
-
-async def start_make_mailing(message: types.Message):
-    if int(message.from_user.id) in managers_id:
-        await MakeMailing.photo.set()
-        await message.reply('Завантажте фото')
+    await message.reply('OK', reply_markup=manager_keyboard)
 
 
 async def add_mailing_photo(message: types.Message, state: FSMContext):
@@ -81,13 +51,14 @@ async def send_mailing(message: types.Message, state: FSMContext):
         mailing_data = DataMailing(photo_id=data['photo_id'], text=data['text'])
         session.add(mailing_data)
         session.commit()
+        await bot.send_message(message.from_user.id, "Повідомлення було відправленно", reply_markup=start_menu)
         await state.finish()
 
 
 async def start_add_pigment(message: types.message):
     if message.from_user.id in managers_id:
         await AddPigment.photo.set()
-        await message.reply('Завантажте фотографію пігменту')
+        await message.reply('Завантажте фотографію пігменту', reply_markup=cancel_markup_admin())
 
 
 async def add_photo_pigment(message: types.Message, state: FSMContext):
@@ -98,8 +69,8 @@ async def add_photo_pigment(message: types.Message, state: FSMContext):
 
 
 async def add_pigment_direction(message: types.Message, state: FSMContext):
-    if message.text not in direction:
-        await bot.send_message(message.from_user.id, 'Введені не вірні данні')
+    if message.text not in direction_pigment:
+        await bot.send_message(message.from_user.id, 'Введені не вірні данні\nВведіть: "Татту" чи "Перманент"')
     else:
         async with state.proxy() as data:
             data['direction'] = message.text
@@ -111,14 +82,7 @@ async def add_zone_or_color(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['zone_or_color'] = message.text
         await AddPigment.next()
-        await message.reply('Введіть виробника')
-
-
-async def add_company_creator(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['company_creator'] = message.text
-    await AddPigment.next()
-    await message.reply('Введіть назву пігменту')
+        await message.reply('Введіть назву пігменту')
 
 
 async def add_pigment_name(message: types.Message, state: FSMContext):
@@ -132,52 +96,62 @@ async def add_description(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['description'] = message.text
     await AddPigment.next()
-    await message.reply('Введіть обьеми та ціни на пігменти. Формат: (Обьем: Ціна)')
+    await message.reply('Введіть об\'єми та ціни на пігменти. Формат: (об\'єм: Ціна)')
 
 
 async def add_volume_and_price(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['volume_and_price'] = message.text
+    await AddPigment.next()
+    await message.reply('Введіть виробника')
 
-    pigment = Pigments(photo=data["photo"], direction=data["direction"], zone_or_color=data["zone_or_color"],
-                       company_creator=data["company_creator"], pigment_name=data["pigment_name"],
-                       description=data["description"], volume_and_price=data["volume_and_price"])
-    session.add(pigment)
-    session.commit()
-    await state.finish()
+
+async def add_company_creator(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['company_creator'] = message.text
+    try:
+        pigment = Pigments(photo=data["photo"], direction=data["direction"], zone_or_color=data["zone_or_color"],
+                           company_creator=data["company_creator"], pigment_name=data["pigment_name"],
+                           description=data["description"], volume_and_price=data["volume_and_price"])
+        session.add(pigment)
+        session.commit()
+        await bot.send_message(message.from_user.id, 'Пігмент додано', reply_markup=manager_keyboard)
+        await state.finish()
+    except IntegrityError:
+        session.rollback()
+        await bot.send_message(message.from_user.id, "Не вірна назва виробника.\n"
+                                                     "Введіть вірну назву знов")
 
 
 async def start_add_creator(message: types.Message):
     if message.from_user.id in managers_id:
-        await AddCreator.photo.set()
-        await message.reply('Завантажте логотип виробника')
-
-
-async def add_photo_creator(messgae: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['photo'] = messgae.photo[0].file_id
-    await AddCreator.next()
-    await messgae.reply('Введіть призначення пігменту("Татту" чи "Перманент")')
+        await AddCreator.direction.set()
+        await message.reply('Введіть призначення пігменту("Татту" чи "Перманент")', reply_markup=cancel_markup_admin())
 
 
 async def add_creator_direction(message: types.Message, state: FSMContext):
-    if message.text not in direction:
+    if message.text not in direction_pigment:
         await bot.send_message(message.from_user.id, 'Введені не вірні данні.\n'
-                                                     'Введіть: "Тутту" чи "Перманент"')
-    async with state.proxy() as data:
-        data['direction'] = message.text
-    await AddCreator.next()
-    await message.reply('Введіть назву виробника')
+                                                     'Введіть: "Татту" чи "Перманент"')
+    else:
+        async with state.proxy() as data:
+            data['direction'] = message.text
+        await AddCreator.next()
+        await message.reply('Введіть назву виробника')
 
 
 async def add_creator_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
-
-    creator = Creator(photo=data['photo'], direction=data['direction'], creator_name=data['name'])
-    session.add(creator)
-    session.commit()
-    await state.finish()
+    try:
+        creator = Creator(direction=data['direction'], creator_name=data['name'])
+        session.add(creator)
+        session.commit()
+        await bot.send_message(message.from_user.id, 'Виробника додано', reply_markup=manager_keyboard)
+        await state.finish()
+    except IntegrityError:
+        session.rollback()
+        await bot.send_message(message.from_user.id, 'Назва виробника зайнята. Введіть іншу назву')
 
 
 async def send_client_keyboard(message: types.Message):
@@ -207,34 +181,31 @@ async def delete_creator(callback: types.CallbackQuery):
 
 
 def register_handler_admin(dp: Dispatcher):
-    # add manager
-    dp.register_message_handler(start_add_manager, commands=admin_commands['Додати_працівника'], state=None)
+    # make a mailing
+    dp.register_message_handler(start_make_mailing, Text(startswith=admin_commands['Зробити_розсилку']), state=None)
+    # cancel handler
     dp.register_message_handler(cancel_admin_handlers, state="*", commands=admin_commands['Відмінити'])
     dp.register_message_handler(cancel_admin_handlers, Text(equals=admin_commands['Відмінити'], ignore_case=True),
                                 state="*")
-    dp.register_message_handler(add_last_name_manager, state=AddManagers.last_name)
-    dp.register_message_handler(add_id_manager, state=AddManagers.manager_id)
-    # make a mailing
-    dp.register_message_handler(start_make_mailing, commands=admin_commands['Зробити_розсилку'], state=None)
+    # and cansel handler
     dp.register_message_handler(add_mailing_photo, content_types=['photo'], state=MakeMailing.photo)
     dp.register_message_handler(send_mailing, state=MakeMailing.text)
     # add pigment
-    dp.register_message_handler(start_add_pigment, commands=admin_commands['Додати_пігмент'], state=None)
+    dp.register_message_handler(start_add_pigment, Text(startswith=admin_commands['Додати_пігмент']), state=None)
     dp.register_message_handler(add_photo_pigment, content_types=['photo'], state=AddPigment.photo)
     dp.register_message_handler(add_pigment_direction, state=AddPigment.direction)
     dp.register_message_handler(add_zone_or_color, state=AddPigment.zone_or_color)
-    dp.register_message_handler(add_company_creator, state=AddPigment.company_creator)
     dp.register_message_handler(add_pigment_name, state=AddPigment.pigment_name)
     dp.register_message_handler(add_description, state=AddPigment.description)
     dp.register_message_handler(add_volume_and_price, state=AddPigment.volume_and_price)
+    dp.register_message_handler(add_company_creator, state=AddPigment.company_creator)
     # add Creator
-    dp.register_message_handler(start_add_creator, commands=admin_commands['Додати_виробника'], state=None)
-    dp.register_message_handler(add_photo_creator, content_types=['photo'], state=AddCreator.photo)
+    dp.register_message_handler(start_add_creator, Text(startswith=admin_commands['Додати_виробника']), state=None)
     dp.register_message_handler(add_creator_direction, state=AddCreator.direction)
     dp.register_message_handler(add_creator_name, state=AddCreator.creator_name)
     # send keyboards
     dp.register_message_handler(send_client_keyboard, commands=admin_commands['Клавіатура_клієнт'])
     dp.register_message_handler(send_manager_keyboard, commands=admin_commands['Клавіатура_менеджера'])
     # delete creator
-    dp.register_message_handler(all_creators, commands=admin_commands['Видалити_виробника'])
+    dp.register_message_handler(all_creators, Text(startswith=admin_commands['Видалити_виробника']))
     dp.register_callback_query_handler(delete_creator, Text(startswith='Видалити-виробника_'))
--- app/handlers/client.py
+++ app/handlers/client.py
@@ -2,16 +2,16 @@ from aiogram.dispatcher.filters import Text
 from datetime import date
 
 from app.create_bot import bot
-from app.keyboards.client_keyboards import start_menu
+from app.keyboards.client_keyboards import start_menu, cancel_markup_client
 from app.keyboards.client_keyboards import basket_menu
 from app.keyboards.admin_keyboards import choose_keyboard
 from app.keyboards.inline import tatoo_and_permanent_inline_button, color_or_zone_inline_button, delete_item, \
-    add_to_basket_markup, choice_tattoo_or_permanent, delete_item_from_basket
+    add_to_basket_markup, choice_tattoo_or_permanent, delete_item_from_basket, choice_any_creator_or_color_or_zone
 
 from app.handlers.handlers_commands import client_commands
-from app.handlers.admin import managers_id, cancel_admin_handlers
+from app.handlers.admin import managers_id
 from app.handlers.state_machines import MakeOrder
-from app.data_base import AllClients, session, Pigments, Creator, Orders
+from app.data_base import AllClients, session, Pigments, Orders
 
 from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram import types
@@ -36,11 +36,11 @@ async def contacts(message: types.Message):
                                                  '+38(096) 648-69-85\n'
                                                  '+38(063) 531-10-93\n'
                                                  '+38(095) 220-11-14\n'
-                                                 'Час роботи: Кожен день з 9:00 до 20:00'
+                                                 'Час роботи: Пн-Вс з 9:00 до 20:00'
                            )
     await bot.send_message(message.from_user.id, 'м.Дніпро, Проспект Дмитра Яворницького 52, ТЦ ЦУМ, 1 поверх\n'
                                                  'Телефон магазину в ТЦ ЦУМ: +38(063) 258-69-03\n'
-                                                 'Час роботи: Кожен день з 9:00 до 20:00')
+                                                 'Час роботи: Пн-Вс з 9:00 до 20:00')
     await bot.send_location(message.from_user.id, '48.465292845969934', '35.04569818039999')
 
 
@@ -49,18 +49,24 @@ async def choice_keyboard_client(message: types.Message):
                            reply_markup=choice_tattoo_or_permanent())
 
 
+async def cancel_client_handlers(message: types.Message, state: FSMContext):
+    if int(message.from_user.id) in managers_id:
+        current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('OK', reply_markup=start_menu)
+
+
 '''
 ########################################Tattoo########################################
 '''
 
 
 async def tatoo_creators(callback: types.CallbackQuery):
-    all_pigments = session.query(Creator).filter_by(direction="Татту").all()
-    for item in all_pigments:
-        print(item.creator_name)
-        await bot.edit_message_reply_markup(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
+    await bot.edit_message_reply_markup(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
                                             reply_markup=tatoo_and_permanent_inline_button(
-                                                "Тату-виробник", item.creator_name, item.creator_name))
+                                                "Тату-виробник", "Татту"))
 
 
 async def tattoo_colors(callback: types.CallbackQuery):
@@ -87,29 +93,28 @@ async def tattoo_pigments(callback: types.CallbackQuery):
     await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
     for pigment in all_pigments_query:
         if callback.from_user.id in managers_id:
-            await bot.send_photo(callback.from_user.id, pigment.photo, f'Назва:{pigment.pigment_name}\n'
-                                                                       f'Опис:{pigment.description}\n'
-                                                                       f'Ціни та обєм:{pigment.volume_and_price}',
-                                 reply_markup=delete_item(pigment.pigment_name, pigment.id), )
+            await bot.send_photo(callback.from_user.id, pigment.photo, f'Назва: {pigment.pigment_name}\n'
+                                                                       f'Опис: {pigment.description}\n'
+                                                                       f'Ціни та обєм: {pigment.volume_and_price}',
+                                 reply_markup=delete_item(pigment.pigment_name, pigment.id))
         else:
             await bot.send_photo(callback.from_user.id, pigment.photo, f'Назва:{pigment.pigment_name}\n'
                                                                        f'Опис:{pigment.description}\n'
-                                                                       f'Ціни та обєм:{pigment.volume_and_price}')
-
-
+                                                                       f'Ціни та обєм:{pigment.volume_and_price}',
+                                 reply_markup=add_to_basket_markup(pigment.pigment_name, pigment.id))
+    await bot.send_message(callback.from_user.id, 'Зробити інший вибір',
+                           reply_markup=choice_any_creator_or_color_or_zone('Пігменти-для-перманенту',
+                                                                            "Перманент-выробник",
+                                                                            callback_data[2]))
 '''
 ########################################Permanent########################################
 '''
 
 
 async def permanent_creators(callback: types.CallbackQuery):
-    all_pigments = session.query(Creator).filter_by(direction="Перманент").all()
-    for item in all_pigments:
-        await bot.edit_message_reply_markup(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
-                                            reply_markup=tatoo_and_permanent_inline_button(
-                                                "Перманент-выробник", item.creator_name, item.creator_name))
-        # await bot.send_photo(message.from_user.id, item.photo, reply_markup=tatoo_and_permanent_inline_button(
-        #     "Перманент-выробник", item.creator_name, item.creator_name))
+    await bot.edit_message_reply_markup(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
+                                        reply_markup=tatoo_and_permanent_inline_button(
+                                            "Перманент-выробник", "Перманент"))
 
 
 async def permanent_zones(callback: types.CallbackQuery):
@@ -135,18 +140,23 @@ async def permanent_pigments(callback: types.CallbackQuery):
         company_creator=callback_data[2],
         zone_or_color=callback_data[3]).all()
     await callback.answer("Завантажую...")
+    await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
     for pigment in all_pigments_permanent_query:
-        await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
         if callback.from_user.id in managers_id:
-            await bot.send_photo(callback.from_user.id, pigment.photo, f'Назва:{pigment.pigment_name}\n'
-                                                                       f'Опис:{pigment.description}\n'
-                                                                       f'Ціни та обєм:{pigment.volume_and_price}',
+            await bot.send_photo(callback.from_user.id, pigment.photo, f'Назва: {pigment.pigment_name}\n'
+                                                                       f'Опис: {pigment.description}\n'
+                                                                       f'Ціни та об\'єм: {pigment.volume_and_price}',
                                  reply_markup=delete_item(pigment.pigment_name, pigment.id))
+
         else:
-            await bot.send_photo(callback.from_user.id, pigment.photo, f'Назва:{pigment.pigment_name}\n'
-                                                                       f'Опис:{pigment.description}\n'
-                                                                       f'Ціни та обєм:{pigment.volume_and_price}',
+            await bot.send_photo(callback.from_user.id, pigment.photo, f'Назва: {pigment.pigment_name}\n'
+                                                                       f'Опис: {pigment.description}\n'
+                                                                       f'Ціни та об\'єм: {pigment.volume_and_price}',
                                  reply_markup=add_to_basket_markup(pigment.pigment_name, pigment.id))
+    await bot.send_message(callback.from_user.id, 'Зробити інший вибір',
+                           reply_markup=choice_any_creator_or_color_or_zone('Пігменти-для-перманенту',
+                                                                            "Перманент-выробник",
+                                                                            callback_data[2]))
 
 
 async def delete_pigment(callback: types.CallbackQuery):
@@ -161,7 +171,6 @@ async def delete_pigment(callback: types.CallbackQuery):
 
 async def add_to_basket(callback: types.CallbackQuery):
     callback_data = callback.data.split('_')
-    print(callback_data)
     try:
         order = session.query(Orders).filter_by(client_id=callback.from_user.id).filter_by(order_status=False).first()
         items_list = list(order.items)
@@ -177,17 +186,24 @@ async def add_to_basket(callback: types.CallbackQuery):
 
 
 async def my_basket(message: types.Message):
-    basket = session.query(Orders).filter_by(client_id=message.from_user.id, order_status=False).first()
-    pigments = session.query(Pigments).all()
-    basket_list = list(basket.items)
-    await bot.send_message(message.from_user.id, 'Ваше замовлення:', reply_markup=basket_menu)
-    for item in basket_list:
-        for pigment in pigments:
-            if pigment.id == item:
-                await bot.send_photo(message.from_user.id, pigment.photo, f'Назва: {pigment.pigment_name}\n'
-                                                                          f'Обьем та ціни: {pigment.volume_and_price}',
-                                     reply_markup=delete_item_from_basket(pigment.id))
-
+    try:
+        basket = session.query(Orders).filter_by(client_id=message.from_user.id, order_status=False).first()
+        a = basket.items[0]
+        pigments = session.query(Pigments).all()
+        basket_list = list(basket.items)
+        await bot.send_message(message.from_user.id, 'Ваше замовлення:', reply_markup=basket_menu)
+        for item in basket_list:
+            for pigment in pigments:
+                if pigment.id == item:
+                    await bot.send_photo(message.from_user.id, pigment.photo, f'Назва: {pigment.pigment_name}\n'
+                                                                              f'Обьем та ціни: {pigment.volume_and_price}',
+                                         reply_markup=delete_item_from_basket(pigment.id))
+    except AttributeError:
+        await bot.send_message(message.from_user.id, "Ваш кошик пустий🥺\n"
+                                                     "За покупками🚀", reply_markup=choice_tattoo_or_permanent())
+    except IndexError:
+        await bot.send_message(message.from_user.id, "Ваш кошик пустий🥺\n"
+                                                     "За покупками🚀", reply_markup=choice_tattoo_or_permanent())
 
 async def delete_from_basket(callback: types.CallbackQuery):
     callback_data = callback.data.split('_')
@@ -205,7 +221,8 @@ async def delete_from_basket(callback: types.CallbackQuery):
 async def start_make_order(message: types.Message):
     await MakeOrder.any_information.set()
     await message.reply('Напишіть кількість та обьеми необхідних пігментів.\n '
-                        'Також ви можете вказати инші товари яки вам необхідні або будь-яку іншу інформацию :) ')
+                        'Також ви можете вказати инші товари яки вам необхідні або будь-яку іншу інформацию :) ',
+                        reply_markup=cancel_markup_client())
 
 
 async def add_more_info(message: types.Message, state: FSMContext):
@@ -248,7 +265,8 @@ async def add_contact_and_send_order(message: types.Message, state: FSMContext):
                                            f'Інформація про замовлення: {data["more_info"]}\n'
                                            f'Замовлення: {items_in_order}')
 
-    await bot.send_message(message.from_user.id, 'Замовлення відпраленно менеджеру, з Вами зв\'яжуться в порядку черги')
+    await bot.send_message(message.from_user.id, 'Замовлення відпраленно менеджеру, з Вами зв\'яжуться в порядку черги',
+                           reply_markup=start_menu)
     await state.finish()
 
 
@@ -268,8 +286,9 @@ def register_handlers_client(dp: Dispatcher):
     dp.register_callback_query_handler(delete_from_basket, Text(startswith="Видалити-з-корзини_"))
     # make order
     dp.register_message_handler(start_make_order, Text(startswith=client_commands['Оформити_замолення']), state=None)
-    dp.register_message_handler(cancel_admin_handlers, state="*", commands=client_commands['Відмінити'])
-    dp.register_message_handler(cancel_admin_handlers, Text(equals=client_commands['Відмінити'], ignore_case=True),
+    dp.register_message_handler(cancel_client_handlers, state="*", commands=client_commands['Відмінити_оформлення'])
+    dp.register_message_handler(cancel_client_handlers, Text(equals=client_commands['Відмінити_оформлення'],
+                                                             ignore_case=True),
                                 state="*")
     dp.register_message_handler(add_more_info, state=MakeOrder.any_information)
     dp.register_message_handler(add_delivery_data, state=MakeOrder.delivery_data)
